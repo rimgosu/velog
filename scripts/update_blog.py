24352009434bfffee2845d1f0198e4041b6d848c
jihyup-byun <120752098+rimgosu@users.noreply.github.com>
2024-03-28 12:41:10 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -38,7 +38,7 @@ for entry in feed.entries:
 
         # 깃허브 커밋
         repo.git.add(file_path)
-        repo.git.commit('-m', f'Add post: {entry.title}')
+        repo.git.commit('-m', f'Add post: {entry.title}\nCo-authored-by: rimgosu <newnyup@gmail.com>')
 
 # 변경 사항을 깃허브에 푸시
 repo.git.push()
